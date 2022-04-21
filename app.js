d756661d0bb00faa838d0144af0d4bf0ce4e2d20
rafik12345 <rafik-08@hotmail.com>
2022-04-21 19:54:50 +0200

    Swagger docs added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 
@@ -23,21 +28,22 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.set('trust proxy', 1);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 app.use(
 	rateLimiter({
     windowMs: 10 * 60 * 1000, // 15 minutes
 		max: 100, // limit each IP to 100 request per windowMs
 	})
 );
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(xss());
 
 
 app.get('/', (req, res) => {
-  res.send('jobs api')
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
